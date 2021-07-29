0574442559dd2f4a9595ce24544f9155a5525779
Steven Black <genseb7@gmail.com>
2021-07-29 16:29:05 -0400

    add outputs for SGs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+
+output "instance_sg_arn" {
+  description = "Security Group ARN attached to instance launch config and thereby the nexus EC2 instance"
+  value       = aws_security_group.elb.arn
+}
+
 output "lb_arn" {
   description = "ARN of the ELB for Nexus access"
   value       = aws_lb.this.arn
@@ -8,6 +14,11 @@ output "lb_dns_name" {
   value       = aws_lb.this.dns_name
 }
 
+output "lb_sg_arn" {
+  description = "Security Group ARN attached to ELB"
+  value       = aws_security_group.elb.arn
+}
+
 output "lb_zone_id" {
   description = "Route53 Zone ID of the ELB for Nexus access"
   value       = aws_lb.this.zone_id
@@ -16,9 +27,4 @@ output "lb_zone_id" {
 output "role_arn" {
   description = "IAM Role ARN of Nexus instance"
   value       = aws_iam_role.this.arn
-}
-
-output "sg_arn" {
-  description = "Security Group ARN"
-  value       = aws_security_group.elb.arn
 }
